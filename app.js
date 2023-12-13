543fa8e5da776a83c94888ad1a9d4f7f7d81ee8e
Sappakorn26256 <45550728+sappakorn@users.noreply.github.com>
2023-12-13 19:02:28 +0700

    productupdate
    แก้ไขโปรหน้า

--- app.js
+++ app.js
@@ -2,34 +2,47 @@ const express = require("express");
 const app = express();
 const port = 3020;
 const pgp = require("pg-promise")();
-const path = require("path");
-const db = pgp("postgres://postgres:1234@localhost:5432/myDB");//เชื่อมต่อฐานข้อมูล
+const path = require("path"); //ใช้สร้างpath
+const { send } = require("process");
+const db = pgp("postgres://postgres:1234@localhost:5432/myDB"); //เชื่อมต่อฐานข้อมูล
+
+const productRouter = express.Router(); //เรียกใช้เราเตอร์ ใช้กับหน้าproduct
 
 app.use(express.static(path.join(__dirname, "/public/")));
+app.set("views","./src/views") ;//เส้นทางการเข้าถึง folder views
+app.set("view engine","ejs");
 
-app.get("/", (req,res)=>{
-  
-})
+/* app.get("/...", (req, res) => {
+  res.render('index', { products: data1 });
+}); */
 
 
-
-app.get("/hello", (req, res) => {
+productRouter.route("/").get((req,res)=>{
   db.any("select * from products")
     .then((data1) => {
-      console.log(data1);
-      return res.status(200).json(data1);
+      res.render('products', { data1: data1 });
     })
     .catch((error1) => {
-      console.log(error1);
       return res.status(400).json(error1);
     });
 });
 
 
 
+app.use("/products", productRouter)
 
 
-
+app.get("/", (req, res) => {
+  db.any("select * from products")
+    .then((data1) => {
+      console.log(data1);
+      res.render('index', { data1: data1 });
+    })
+    .catch((error1) => {
+      console.log(error1);
+      return res.status(400).json(error1);
+    });
+});
 
 
 
@@ -44,4 +57,3 @@ app.listen(port, () => {
     console.log(error);
   }
 }); */
-
